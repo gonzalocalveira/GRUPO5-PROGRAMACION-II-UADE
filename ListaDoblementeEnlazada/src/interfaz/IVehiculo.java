d14e1ba6ea32dbf850e5ab05df3bfda5fcb220d6
gonzalocalveira <gcalveira27@gmail.com>
2025-04-22 18:49:10 -0300

    Update IVehiculo.java

--- ListaDoblementeEnlazada/src/interfaz/IVehiculo.java
+++ ListaDoblementeEnlazada/src/interfaz/IVehiculo.java
@@ -1,12 +1,24 @@
 package interfaz;
 
 public interface IVehiculo {
-	
+	/// Precondición: ninguna.
+	/// Postcondición: Devuelve la patente del vehículo.
 	public String getPatente();
+
+	/// Precondición: patenteNueva no debe ser null.
+	/// Postcondición: La patente del vehículo se actualiza.
 	public void setPatente(String patenteNueva);
-	
+
+	/// Precondición: ninguna.
+	/// Postcondición: Devuelve la marca del vehículo.
 	public String getMarca();
+	
+	/// Precondición: marcaNueva no debe ser null.
+	/// Postcondición: La marca del vehículo se actualiza.
 	public void setMarca(String marcaNueva);
+
+	/// Precondición: ninguna.
+	/// Postcondición: Devuelve una representación textual del vehículo.
 	public String toString(); 
 	
 	
