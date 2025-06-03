71a11855e819705e1437f87c21c61922e6643987
Gerocraken <geronimogarat@gmail.com>
2025-06-03 10:42:16 -0300

    Update TestArbolRecorridoPreOrden.java

--- TP2/src/testeo/TestArbolRecorridoPreOrden.java
+++ TP2/src/testeo/TestArbolRecorridoPreOrden.java
@@ -7,11 +7,11 @@ import modelo.Persona;
 
 public class TestArbolRecorridoPreOrden {
 	public static void main(String[] args) {
-		IPersona p1= new Persona("Julian","45675291");
-		IPersona p2= new Persona("Di Maria", "26789786");
-		IPersona p3= new Persona("Roberto", "98780923");
-		IPersona p4= new Persona("Hazard", "56789098");
-		IPersona p5= new Persona("Miguel", "12345678");
+		IPersona p1= new Persona("Julian","45675251");
+		IPersona p2= new Persona("Di Maria", "26789987");
+		IPersona p3= new Persona("Roberto", "50780923");
+		IPersona p4= new Persona("Hazard", "86489098");
+		IPersona p5= new Persona("Miguel", "12345978");
 		
 		
 		IArbol arbol= new Arbol();
