9019ca1afec1ae924d64f2fc01d31791fa961d41
gonzalocalveira <gcalveira27@gmail.com>
2025-06-03 10:17:50 -0300

    Agrego metodo "prostOredenRecu" y modifico el metodo "recorridoPostOrden"

--- TP2/src/interfaz/IArbol.java
+++ TP2/src/interfaz/IArbol.java
@@ -5,14 +5,22 @@ import interfaz.IPersona;
 public interface IArbol<T> {
 	
 	public INodo insertarRecu( INodo<T> nodo,  T dato);
+	
 	public T eliminar(T dato);
+	
 	public void insertar(T dato);
+	
 	public void recorridoPreOrden(INodo nodo);
 	public void recorridoPre_Orden();
+	
 	public void inordenRec(INodo nodo);
 	public void recorridoInorden();
-	public void recorridoPostOrden(INodo nodo);
+	
+	public void recorridoPostOrden(INodo<T> nodo);
+        public void postOrdenRecu(INodo<T> nodo);
+	
 	public int comparar(INodo<T> nodo, T dato);
+	
 	public INodo<T> buscar(T dato);
 	
 	
