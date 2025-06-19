4514423f37c5dd2db378730e740da50a1f28c2e1
gonzalocalveira <gcalveira27@gmail.com>
2025-06-19 11:57:23 -0300

    Update IPersona.java

--- TP3/src/interfaz/IPersona.java
+++ TP3/src/interfaz/IPersona.java
@@ -8,6 +8,5 @@ public interface IPersona extends Comparable<IPersona>{
 	public String getNombre();
 	public String getDNI();
 	public String toString();
-	public int compareTo(IPersona otraPersona);
-	public void ordenarLista(List<IPersona> p);
+	
 }
