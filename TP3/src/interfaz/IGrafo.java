480b75a09c9beb7430cb468df8257e153322c008
gonzalocalveira <gcalveira27@gmail.com>
2025-06-19 09:54:37 -0300

    Actualizar IGrafo.java

--- TP3/src/interfaz/IGrafo.java
+++ TP3/src/interfaz/IGrafo.java
@@ -5,6 +5,13 @@ public interface IGrafo<T> {
 	public void agregarNodo(T dato);
 	
 	public void agregarArista(T inicio, T fin);
+ 
+ public void recorridoBFS(T inicio);
+
+ public void recorridoDFS(T inicio);
+
+
+ public void matrizAdyacencia();
 	
 
 }
