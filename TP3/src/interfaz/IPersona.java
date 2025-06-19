0cff469a2e4879a8154f1d8f380325bee8e8b48b
gonzalocalveira <gcalveira27@gmail.com>
2025-06-19 12:13:13 -0300

    Update IPersona.java

--- TP3/src/interfaz/IPersona.java
+++ TP3/src/interfaz/IPersona.java
@@ -4,9 +4,28 @@ import java.util.List;
 
 public interface IPersona extends Comparable<IPersona>{
 	
-	
+	/**
+     * Devuelve el nombre de la persona.
+     *
+     * Precondición: Ninguna.
+     * Postcondición: Se retorna el valor del atributo `nombre`.
+     */
 	public String getNombre();
+
+/**
+     * Devuelve el DNI de la persona.
+     *
+     * Precondición: Ninguna.
+     * Postcondición: Se retorna el valor del atributo `DNI`.
+     */
 	public String getDNI();
+
+/**
+     * Devuelve una representación en forma de cadena del objeto Persona.
+     *
+     * Precondición: Ninguna.
+     * Postcondición: Se retorna un String con los valores de nombre y DNI.
+     */
 	public String toString();
 	
 }
